014904cf29f44cf65d1c47496d66606a37a4ab42
Skdiy <amogrovejom@unsa.edu.pe>
2021-11-04 12:36:16 -0500

    test GA

--- src/App.js
+++ src/App.js
@@ -1,19 +1,51 @@
 import React, { useEffect } from "react";
-import ReactGa from "react-ga";
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AppOnePage from "./AppOne";
+import AppTwoPage from "./AppTwo";
+import ReactGA from "react-ga";
 
 function App() {
   useEffect(() => {
-    ReactGa.initialize("G-K91CNEY18G");
-    ReactGa.pageview(window.location.pathname + window.location.search);
+    ReactGA.initialize("UA-211919696-1");
+    // To Report Page View
+    ReactGA.pageview(window.location.pathname + window.location.search);
   }, []);
+
+  useEffect(() => {
+    console.log(window.location.pathname);
+  });
+
+  const ClickHandler = () => {
+    ReactGA.event({
+      category: "Button",
+      action: "Se apreto el boton de",
+    });
+    alert(
+      "Send the information to google analystic that I touched the click button"
+    );
+  };
 
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <button onClick={ClickHandler}>Click</button>
+        <Router>
+          <a href="/appOne">AppOne</a>
+          <br />
+          <a href="/appTwo">AppTwo</a>
+          <br />
+          <br />
+
+          <Routes>
+            <Route exact path="/appOne">
+              <AppOnePage />{" "}
+            </Route>
+            <Route exact path="/appTwo">
+              <AppTwoPage />{" "}
+            </Route>
+          </Routes>
+        </Router>
       </header>
     </div>
   );
